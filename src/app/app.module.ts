fb76f4e6402d37f435ac25ef54e12572e946da88
Tati <tatiestrada@me.com>
2017-02-03 13:21:23 +0000

    contact component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,16 @@ import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { routing } from './app.routing';
+import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
 
 
 
 
 @NgModule({
   declarations: [ 
-    AppComponent, NavbarComponent, JumbotronComponent, HomeComponent, AboutComponent
+    AppComponent, NavbarComponent, JumbotronComponent, HomeComponent, AboutComponent, ContactComponent, ContactComponent, ContactComponent
   ],
   imports: [
     BrowserModule,
